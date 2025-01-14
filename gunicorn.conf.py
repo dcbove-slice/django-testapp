80f93077545815373c561e362fefbc9f00d74113
Daniel Bove <daniel.bove@slicelife.com>
2025-01-14 18:00:07 -0500

    added gunicorn, whitenoise, and fixed the name of the proxy server when running standalone

---
+++ gunicorn.conf.py
@@ -0,0 +1,5 @@
+bind = "0.0.0.0:8000"  # Bind to all on port 8000
+workers = 3  # Number of worker processes
+timeout = 30  # Worker timeout in seconds
+accesslog = "-"  # Log access to stdout
+errorlog = "-"  # Log errors to stdout
